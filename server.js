5934aec3c2a2ad319ab46d9e87d609bd89998315
giuseppetanzi <giuse00@gmail.com>
2017-01-09 20:09:04 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,11 @@
-var express = require( 'express' )
-    , https = require("https")
-    , fs = require( 'fs' );
+const https = require('https');
 
-var privateKey = process.env.NODE_KEY;
-var certificate = process.env.NODE_CRT;
+const options = {
+  key: process.env.NODE_KEY,
+  cert: process.env.NODE_CRT
+};
 
-var options = {key: privateKey, cert: certificate};
-
-var app = express.createServer();
-
-
-app.get('/', function (req, res) {
-  res.send('Middleware server is running on port \n' + PORT);
-});
-
-
-https.createServer( options, function(req,res)
-{
-    app.handle( req, res );
-} ).listen( 8080 );
+https.createServer(options, (req, res) => {
+  res.writeHead(200);
+  res.end('hello world\n');
+}).listen(8080);
