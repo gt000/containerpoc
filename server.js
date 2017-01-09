7310ff77072a857974be2aeac8cdb8147b6a01a3
giuseppetanzi <giuse00@gmail.com>
2017-01-09 17:19:23 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
-var http = require('http');
+var http = require('http'),crypto = require('crypto');
+
+
+var privateKey = process.env.NODE_KEY;
+var certificate = process.env.NODE_CRT;
+
+var credentials = crypto.createCredentials({key: privateKey, cert: certificate});
 
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
@@ -6,6 +12,8 @@ var server = http.createServer(function (request, response) {
   response.end("Hello World\n");
 });
 
+server.setSecure(credentials);
+server.addListener("request", handler);
 // Listen on port 8000, IP defaults to 127.0.0.1
 server.listen(8080);
 
