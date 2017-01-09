f6e6b67a3630ba49a778957ed06986ea016596d2
giuseppetanzi <giuse00@gmail.com>
2017-01-09 17:32:41 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-var http = require('http'),crypto = require('crypto');
-
+var express = require( 'express' )
+    , https = require("https")
+    , fs = require( 'fs' );
 
 var privateKey = process.env.NODE_KEY;
 var certificate = process.env.NODE_CRT;
 
-var credentials = crypto.createCredentials({key: privateKey, cert: certificate});
+var options = {key: privateKey, cert: certificate};
 
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\n");
+var app = express.createServer();
+
+
+app.get('/', function (req, res) {
+  res.send('Middleware server is running on port \n' + PORT);
 });
 
-server.setSecure(credentials);
-server.addListener("request", handler);
-// Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(8080);
 
-// Put a friendly message on the terminal
-console.log("Server running at http://127.0.0.1:8080/");
+https.createServer( options, function(req,res)
+{
+    app.handle( req, res );
+} ).listen( 8080 );
